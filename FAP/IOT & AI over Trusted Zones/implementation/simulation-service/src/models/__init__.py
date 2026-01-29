084c98620e7da6652cb542995ddb6767155a97b9
Daniel Pires <d.pires@atlas-ios.de>
2026-01-29 20:57:02 -0100

    feat(meter): implement energy meter simulator

--- FAP/IOT & AI over Trusted Zones/implementation/simulation-service/src/models/__init__.py
+++ FAP/IOT & AI over Trusted Zones/implementation/simulation-service/src/models/__init__.py
@@ -1,1 +1,9 @@
 # Pydantic Data Models
+
+from src.models.meter import MeterConfig, MeterReading, MeterReadings
+
+__all__ = [
+    "MeterConfig",
+    "MeterReading",
+    "MeterReadings",
+]
--- FAP/IOT & AI over Trusted Zones/implementation/simulation-service/src/simulators/energy_meter/__init__.py
+++ FAP/IOT & AI over Trusted Zones/implementation/simulation-service/src/simulators/energy_meter/__init__.py
@@ -1,1 +1,28 @@
-# Energy Meter Simulator (Modbus TCP)
+# Energy Meter Simulator
+
+from src.simulators.energy_meter.load_curves import (
+    WEEKDAY_LOAD_CURVE,
+    WEEKEND_LOAD_CURVE,
+    DayType,
+    calculate_power_from_load_factor,
+    distribute_power_across_phases,
+    get_day_type,
+    get_load_factor,
+    get_load_factor_with_noise,
+)
+from src.simulators.energy_meter.simulator import EnergyMeterSimulator, EnergyState
+
+__all__ = [
+    # Simulator
+    "EnergyMeterSimulator",
+    "EnergyState",
+    # Load curves
+    "DayType",
+    "get_day_type",
+    "get_load_factor",
+    "get_load_factor_with_noise",
+    "calculate_power_from_load_factor",
+    "distribute_power_across_phases",
+    "WEEKDAY_LOAD_CURVE",
+    "WEEKEND_LOAD_CURVE",
+]
